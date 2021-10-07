b33c9f5ebb59d8fbc14bdb51187cc6d3ce870d26
JuanMC00 <Juan.Munoz7@alu.uclm.es>
2021-10-07 14:20:42 +0100

    Planetas agregados

--- src/menu.py
+++ src/menu.py
@@ -13,12 +13,27 @@ class Aplicacion:
         self.canvas.pack(expand=True, fill= BOTH)
         background= ImageTk.PhotoImage(file="src/images/background.png")
         self.canvas.create_image(0, 0, image=background, anchor="nw")
+        
+        #Creacion de planetas
+        tierra_image = ImageTk.PhotoImage(file="src/images/tierra.png")
+        self.ovni=self.canvas.create_image(50, 150, image=tierra_image, anchor="w")
+
+        venus_image = ImageTk.PhotoImage(file="src/images/venus.png")
+        self.ovni=self.canvas.create_image(100, 300, image=venus_image, anchor="w")
+
+        marte_image = ImageTk.PhotoImage(file="src/images/marte.png")
+        self.ovni=self.canvas.create_image(200, 50, image=marte_image, anchor="w")
+
+        urano_image = ImageTk.PhotoImage(file="src/images/urano.png")
+        self.ovni=self.canvas.create_image(500, 350, image=urano_image, anchor="w")
+
 
         #Creamos la entidad que se desplaza por el lobby
         ovni_image = ImageTk.PhotoImage(file="src/images/ovni.png")
-        self.ovni=self.canvas.create_image(200, 200, image=ovni_image, anchor="w")
+        self.ovni=self.canvas.create_image(500, 300, image=ovni_image, anchor="w")
         self.ventana.bind("<KeyPress>", self.presion_tecla)
-        
+
+
         self.ventana.resizable(False, False)
         self.ventana.mainloop()
 
