2763b75b3458a6068734b00dd9408f11614cd644
JuanMC00 <Juan.Munoz7@alu.uclm.es>
2021-10-14 10:03:37 +0100

    Creando labels para planetas

--- src/menu.py
+++ src/menu.py
@@ -15,17 +15,32 @@ class Aplicacion:
         self.canvas.create_image(0, 0, image=background, anchor="nw")
         
         #Creacion de planetas
-        tierra_image = ImageTk.PhotoImage(file="src/images/tierra.png")
-        self.ovni=self.canvas.create_image(50, 150, image=tierra_image, anchor="w")
+        tierra_image = ImageTk.PhotoImage(file="src/images/tierra.gif")
+        self.tierra = Label(self.canvas, image=tierra_image)
+        self.tierra.place(x=25, y=125, bordermode="outside")
+
+
+        #self.tierra = self.canvas.create_image(25, 125, image=tierra_image, anchor="w")
+        #tierra = Button(self.canvas, image = tierra_image, background=)
+
+        
+        
+
+
+        #self.ventana.wm_attributes('-transparentcolor', self.ventana['bg'])
+
 
         venus_image = ImageTk.PhotoImage(file="src/images/venus.png")
-        self.ovni=self.canvas.create_image(100, 300, image=venus_image, anchor="w")
+        self.venus=self.canvas.create_image(25, 450, image=venus_image, anchor="w")
 
         marte_image = ImageTk.PhotoImage(file="src/images/marte.png")
-        self.ovni=self.canvas.create_image(200, 50, image=marte_image, anchor="w")
+        self.marte=self.canvas.create_image(400, 125, image=marte_image, anchor="w")
 
         urano_image = ImageTk.PhotoImage(file="src/images/urano.png")
-        self.ovni=self.canvas.create_image(500, 350, image=urano_image, anchor="w")
+        self.urano=self.canvas.create_image(750, 125, image=urano_image, anchor="w")
+
+        agujero_image = ImageTk.PhotoImage(file="src/images/agujeroNegro.png")
+        self.agujero=self.canvas.create_image(750, 475, image=agujero_image, anchor="w")
 
 
         #Creamos la entidad que se desplaza por el lobby
@@ -37,6 +52,13 @@ class Aplicacion:
         self.ventana.resizable(False, False)
         self.ventana.mainloop()
 
+    def on_enter(self, event):
+        print("Hola")
+
+    def check_position(self):
+        print()
+
+
     def presion_tecla(self, evento):
         if evento.keysym=='Right':
             self.canvas.move(self.ovni, 10, 0) #Aquí se ajusta la velocidad
@@ -47,5 +69,9 @@ class Aplicacion:
         if evento.keysym=='Up':
             self.canvas.move(self.ovni, 0, -10)
 
+        #check_position()
 
-aplicacion1=Aplicacion()
+    
+
+
+aplicacion=Aplicacion()
