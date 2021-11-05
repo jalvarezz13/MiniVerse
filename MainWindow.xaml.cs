7556853a3bc69e5209c0bc93df9fb61f76e374e6
Juan Muñoz Calvo <Juan.Munoz7@alu.uclm.es>
2021-11-05 18:58:38 +0100

    Movimiento de la nave y un planeta agregado

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,16 +21,57 @@ namespace MiniVerse
     public partial class MainWindow : Window
     {
         public MainWindow()
-        {            
-            InitializeComponent();            
+        {
+            InitializeComponent();
         }
 
         private void moverPersonaje(object sender, KeyEventArgs e)
         {
-            int margenizq;
-            int margenalto;
-            int margendcho;
-            imgPersonaje.Margin = new Thickness(40, 16, 0, 0);
+            int left = (int)imgPersonaje.Margin.Left;
+            int up = (int)imgPersonaje.Margin.Top;
+            int right = (int)imgPersonaje.Margin.Right;
+            int down = (int)imgPersonaje.Margin.Bottom;
+            int velocidad = 15;
+
+            switch (e.Key)
+            {
+                case Key.Left:
+                    if (left - velocidad * 0.5 > 0)
+                        imgPersonaje.Margin = new Thickness(left - velocidad, up, right + velocidad, down);
+                    break;
+
+                case Key.Up:
+                    if (up + velocidad * 1.5 > 0)
+                        imgPersonaje.Margin = new Thickness(left, up - velocidad, right, down + velocidad);
+                    break;
+
+                case Key.Right:
+                    if (right - velocidad * 0.5 > 0)
+                        imgPersonaje.Margin = new Thickness(left + velocidad, up, right - velocidad, down);
+                    break;
+
+                case Key.Down:
+                    if (down - velocidad > 0)
+                        imgPersonaje.Margin = new Thickness(left, up + velocidad, right, down - velocidad);
+                    break;
+            }
+            Console.WriteLine("Estoy fuera");
+            visitarPlanetas(left + 75, up + 75, right + 75, down + 75);
+
+        }
+
+        private void visitarPlanetas(int left, int up, int right, int down)
+        {
+            if (left > 150 && right - 75 > 675 && up > 50 && down > 335)
+            {
+                Console.WriteLine("estoy en venus");
+            }
+
+            else if (left > 925 && right > 150 && up > 350 && down > 30)
+            {
+                Console.WriteLine("Salir");
+                //App.Current.Shutdown();
+            }
         }
     }
 }
